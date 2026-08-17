bac2b516e26d9ed5b7b5a674e4b5d9413f8eefeb
Preston Workstation <me@prestonarnold.uk>
2026-08-17 21:39:24 +0000

    book type

---
+++ backend/src/models/Book.ts
@@ -0,0 +1,23 @@
+export type Status = "currenly_reading" | "finished" | "abandoned";
+
+export interface Book {
+    id: number;
+    userId: number;
+    title: string;
+    author: string;
+    isbn: string | null;
+    coverUrl: string | null;
+    description: string | null;
+    publisher: string | null;
+    publishedDate: string | null;
+    pageCount: number | null;
+    status: Status;
+    startedAt: string | null;
+    finishedAt: string | null;
+    rating: number | null;
+    review: string | null;
+    notes: string | null;
+    isPublic: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
