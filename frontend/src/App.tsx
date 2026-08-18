e331edac48f65103d646fd2ef71e10cdf1879e1a
Preston Workstation <me@prestonarnold.uk>
2026-08-18 11:15:46 +0000

    setup router and query

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div>Hello, Booker!</div>
-  )
-}
-
-export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import './index.css'
-import App from './App.tsx'
+import { routeTree } from './routeTree.gen.ts';
+
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: queryClient,
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
